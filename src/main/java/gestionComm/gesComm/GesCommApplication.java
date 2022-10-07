d2d60505763a8086ff4a5c335719fa69a3bcaa38
HP <latioue@gmail.com>
2022-10-07 10:22:37 +0000

    Initial commit

---
+++ src/main/java/gestionComm/gesComm/GesCommApplication.java
@@ -0,0 +1,13 @@
+package gestionComm.gesComm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesCommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesCommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestionComm/gesComm/GesCommApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionComm.gesComm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesCommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
